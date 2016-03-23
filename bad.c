d07f8c4a9eb7a08c940efc31fd5b662cf46f5282
Darwin <darwin78913@gmail.com>
2016-03-23 14:55:54 -0400

    THIS IS THE CORRECT ONE ETHAN OK????? YOU HAPPY?

--- bad.c
+++ bad.c
@@ -1,5 +1,14 @@
+/**
+
+
+
+**/
+
+
+
 #include <stdio.h>
 #include <unistd.h>
+
 
 #define IFS = 0;
 
@@ -11,22 +20,47 @@ const int rightStart = 0;
 const int rightEnd = 0;
 const int middle = 0;
 
+const int correctTime = 100;
+const int multMult = 0.01;
+int currentTime = 0;
+
 int  failureCount = 0;
 bool running = true;
 bool movingState = 0;
 
+int speedMult = 1;
+int steerMult = 1;
+
 while (running) {
-	bool leftDetect = false;
-	bool rightDetect = false;
+	currentTime++;
+
+	bool leftDetect = 	(sensorInput >= leftStart ) && (sensorInput <= leftEnd );
+	bool rightDetect = 	(sensorInput >= rightStart) && (sensorInput <= rightEnd);
 	bool messUp = false;
 
 	if (startDetect) {
+		//0 = hasn't reached start yet; 1 = has hit starting line; 2 = passed starting line
+		//although we could have just placed the robot on the starting line 
 		if (movingState = 0) movingState = 1;
-		else [dsfsdfsd];
+		//End and set
+		else if (movingState = 2) {
+			running = false;
+			speedMult = correctTime/currentTime;
+		}
+	else if(movingState = 1) movingState = 2;
 
 	} else {
-		if (leftDetect)  { messUp = true; print("Error: Veered Left" ); }
-		if (rightDetect) { messUp = true; print("Error: Veered Right"); }
+		//Detect and correct if move to side
+		if (leftDetect)  {
+			messUp = true;
+			print("Error: Veered Left" ); 
+			steerMult = currentTime * multMult;
+		}
+		if (rightDetect) {
+			messUp = true;
+			print("Error: Veered Right");
+			steerMult = currentTime * multMult *-1;
+		}
 	}
 
 	if(isMoving && !messUp) {
