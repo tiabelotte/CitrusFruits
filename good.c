ff0e7a446e396cca00486c20bafab2cdd4713f1b
jrfarah <jrfarah1999@gmail.com>
2016-04-05 15:05:09 -0400

    update good.c
    latest version of matt damon

--- good.c
+++ good.c
@@ -1,73 +1,123 @@
-/*Code written by Joseph Farah (sometime in March)
 /*Including header files*/
-/*Code for Matt Damon*/
 #include <kipr/botball.h>
 #include <stdio.h>
 /*Defining motor and servo ports*/
-/*Motor ports*/
 #define LM 0
 #define RM 1
-/*Servo ports*/
 #define SVO 0
 #define RSVO 1
 #define LSVO 2
-/*Speeds*/
+#define MSVO 3
 #define HS 100
 #define NS 80
 #define TS -10
 #define PS -100
-/*How many iterations to turn in a perfect right angle*/
-#define right_angle 170
+#define right_angle 190
 /*Function prototypes*/
-/*See function definitions for descriptions*/
 void drive_forwards_fast(int time);
 void drive_forwards_slow(int time);
 void drive_backwards_fast(int time);
+void drive_backwards_slow(int time);
 void turn_left(int time);
 void turn_right(int time);
 void pivot_left(int time);
 void pivot_right(int time);
-void line_follow(int threshold, int time);
+void line_followL(int threshold, int time);
+void line_followR(int threshold, int time);
+void line_follow_backwardsL(int threshold, int time);
+void line_follow_backwardsR(int threshold, int time);
+void aosleep(int ms);
+
 
 /*Main Function*/
+
 int main() {
-  /*Initializing the threshold of white-black; 
-  number subject to change*/
+  //Setup
+  //Line following threshold
   int current_threshold = 2500;
   enable_servos();
-  /*Raise the arm just above the solar arrays 
-  and prepare to grasp them*/
+  
+  //Grab with arm//
   set_servo_position(SVO,310);
   set_servo_position(LSVO,931);
   set_servo_position(RSVO,968);
-  msleep(1000);
-  /*Drive onto the solar arrays*/
-  drive_forwards_fast(8);
-  /*Grasp the arrays*/
+  set_servo_position(MSVO,1015);
+  aosleep(1000);
+  
+  //Move out of box//
+  drive_forwards_slow(150); //Driving out of the starter box
   set_servo_position(SVO,50);
   set_servo_position(LSVO,1604);
   set_servo_position(RSVO,312);
-  msleep(1000);
-  /*Lift them in "balanced position"*/
+  aosleep(200);
+  set_servo_position(SVO,350);
+  aosleep(150);
   set_servo_position(SVO,750);
-  /*Drive up to the slope*/
-  pivot_right(right_angle);
-  drive_forwards_fast(330);
-  pivot_left(right_angle);
-  drive_forwards_fast(700);
-  /*Line follow to secure position*/
-  line_follow(current_threshold,280);
-  drive_forwards_fast(300);
+  aosleep(300);
+  drive_forwards_slow(200); //Driving w/arm up
+  
+  //Turn and drive to wall//
+  pivot_right(right_angle); //Turn towards center of the board
+  aosleep(100);
+  while (digital(0)!=1) {
+    drive_forwards_fast(1); //drive towards pvc
+  }
+  aosleep(250);
+  
+  //Reorient to ramp//
+  drive_backwards_fast(100);
+  pivot_left(right_angle-20); //Turn vaguely towards the ramp
+  aosleep(70);
+  //drive_backwards_fast(25);
+  //aosleep(500);
+  
+  //Drive up ramp//
+  drive_forwards_fast(250); //Drive towards the ramp
+  
+  while(digital(0)!=1) {
+    line_followR(current_threshold,5); //Line follows until it hits the PVC on top of the ramp
+  }
+  
+  //Reverse and turn towards boxes//
+  drive_backwards_fast(75); //reverse
+  aosleep(250);
+  
+  pivot_right(190); //turn towards opposite side of board
+  aosleep(50);
+  pivot_right(35);
+  aosleep(50);
+  
+  while (digital(0)!=1) {
+    drive_forwards_fast(1);
+  }
+  aosleep(50);
+  set_servo_position(MSVO,1401);
+  
+  //Drive back, then line follow//
+  drive_backwards_fast(25);
+  set_servo_position(SVO,1475);
+  line_follow_backwardsL(current_threshold,150);
+  set_servo_position(MSVO,277);
+  msleep(1000);
+  
+  
+  
+  
+  /*
+  
+  set_servo_position(SVO,390);
+  aosleep(1000);
+  set_servo_position(LSVO,861);
+  set_servo_position(RSVO,1221);
+*/
   return 0;
-  
-  
-  
-  
+
+
+
 }
 
 
 /*Function declarations*/
-/*Driving forwards fast*/
 void drive_forwards_fast(int time) {
   int i = 0;
   while(i++<=time) {
@@ -75,7 +125,6 @@ void drive_forwards_fast(int time) {
     motor(RM,HS);
   }
 }
-/*Driving forwards at about two thirds of "fast"*/
 void drive_forwards_slow(int time) {
   int i = 0;
   while(i++<=time) {
@@ -83,15 +132,20 @@ void drive_forwards_slow(int time) {
     motor(RM,NS);
   }
 }
-/*Driving backwards fast*/
 void drive_backwards_fast(int time) {
   int i = 0;
   while(i++<=time) {
-    motor(LM,PS);
-    motor(RM,PS);
+    motor(LM,-HS);
+    motor(RM,-HS);
   }
 }
-/*Turning to the left*/
+void drive_backwards_slow(int time) {
+  int i = 0;
+  while(i++<=time) {
+    motor(LM,-NS);
+    motor(RM,-NS);
+  }
+}
 void turn_left(int time) {
   int i = 0;
   while(i++<=time) {
@@ -99,7 +153,6 @@ void turn_left(int time) {
     motor(LM,TS);
   }
 }
-/*Turning to the right*/
 void turn_right(int time) {
   int i = 0;
   while(i++<=time) {
@@ -107,7 +160,6 @@ void turn_right(int time) {
     motor(LM,HS);
   }
 }
-/*Pivot on point--NOT turning*/
 void pivot_left(int time) {
   int i = 0;
   while(i++<=time) {
@@ -122,21 +174,84 @@ void pivot_right(int time) {
     motor(RM,PS);
   }
 }
-/*Line follow: arguments are whatever threshold we are using, 
-plus however many iterations we want the function to perform*/
-void line_follow(int threshold, int time) {
+void line_followL(int threshold, int time) {
   int i = 0;
   while(i++<time) {
-    if(analog(0)>=threshold) {
-      turn_right(3);
-    }
     if(analog(0)<=threshold) {
-      turn_left(3);
+      for(i=0; i<5; i++) {
+        motor(LM,100);
+        motor(RM,65);
+      } 
+    }
+    if(analog(0)>=threshold) {
+      int i=0;
+      for(i=0; i<5; i++) {
+        motor(RM,100);
+        motor(LM,65);
+      }   
+    }
+  }
+}
+void line_followR(int threshold, int time) {
+  int i = 0;
+  while(i++<time) {
+    if(analog(0)<=threshold) {
+      for(i=0; i<5; i++) {
+        motor(RM,100);
+        motor(LM,65);
+      } 
+    }
+    if(analog(0)>=threshold) {
+      int i=0;
+      for(i=0; i<5; i++) {
+        motor(LM,100);
+        motor(RM,65);
+      }   
+    }
+  }
+}
+void line_follow_backwardsL(int threshold, int time) {
+  int i = 0;
+  while(i++<time) {
+    if(analog(0)<=threshold) {
+      for(i=0; i<5; i++) {
+        motor(LM,-100);
+        motor(RM,-65);
+      } 
+    }
+    if(analog(0)>=threshold) {
+      int i=0;
+      for(i=0; i<5; i++) {
+        motor(RM,-100);
+        motor(LM,-65);
+      }   
+    }
+  }
+}
+void line_follow_backwardsR(int threshold, int time) {
+  int i = 0;
+  while(i++<time) {
+    if(analog(0)<=threshold) {
+      for(i=0; i<5; i++) {
+        motor(RM,-100);
+        motor(LM,-65);
+      } 
+    }
+    if(analog(0)>=threshold) {
+      int i=0;
+      for(i=0; i<5; i++) {
+        motor(LM,-100);
+        motor(RM,-65);
+      }   
     }
   }
 }
 
-
+//Stop & sleep
+void aosleep(int ms) {
+  ao();
+  msleep(ms);
+}
 
 
 
